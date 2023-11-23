588acd7001ac9bc8e107f9f8365c2f09bcc539b4
maiyoj <valentinemaiyo.dev@gmail.com>
2023-11-23 09:48:25 +0300

    change navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,8 +10,8 @@ const Navbar = () => {
     //     <li><Link to="/contact">Contact</Link></li>
     //   </ul>
     // </nav>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Hotel Booking</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -22,11 +22,11 @@ const Navbar = () => {
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">About</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          Service
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="#">Action</a>
@@ -40,7 +40,7 @@ const Navbar = () => {
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      {/* <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/> */}
+      {/* <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> */}
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Hello World</h1>
-      <button className="btn btn-primary">Click me</button>
+      {/* <h1>Hello World</h1>
+      <button className="btn btn-primary">Click me</button> */}
     </div>
   );
 };

e3aaa1b0585999c3f62412e040c1c0d5be43a0f2
maiyoj <valentinemaiyo.dev@gmail.com>
2023-11-25 23:31:37 +0300

    update the home page header section with content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
     // </nav>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">HillTech</a>
+    <a class="navbar-brand" href="#">Hotel Booking</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>

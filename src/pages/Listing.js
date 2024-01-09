9c86881df8612a563cb9902b1f52537fddd86596
maiyoj <valentinemaiyo.dev@gmail.com>
2024-01-09 22:12:47 +0300

    add pagination

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -1,3 +1,254 @@
+// import React, { useState, useEffect, useContext } from "react";
+// import Navbar from "../components/Navbar";
+// import { useLocation, Link } from "react-router-dom";
+// import { HotelContext } from "../context/HotelContext";
+// import Footer from "../components/Footer";
+
+// const Listing = () => {
+//   const location = useLocation();
+//   const queryParams = new URLSearchParams(location.search);
+//   const countryFilter = queryParams.get("country") || "";
+
+//   const [listings, setListings] = useState([]);
+//   const [originalListings, setOriginalListings] = useState([]);
+//   const [cityFilter, setCityFilter] = useState("");
+//   const [suggestions, setSuggestions] = useState([]);
+
+//   const { selectHotel, selectedHotel } = useContext(HotelContext);
+
+//   const handleSelectHotel = (hotelId) => {
+//     selectHotel({ hotelId: hotelId });
+//   };
+
+//   const handleInputChange = async (e) => {
+//     const value = e.target.value;
+//     setCityFilter(value);
+
+//     try {
+//       const response = await fetch("https://restcountries.com/v3.1/all");
+
+//       if (response.ok) {
+//         const data = await response.json();
+//         const countryNames = data.map((country) => country.name.common);
+//         setSuggestions(
+//           countryNames.filter((country) =>
+//             country.toLowerCase().startsWith(value.toLowerCase())
+//           )
+//         );
+//       } else {
+//         throw new Error("Failed to fetch data");
+//       }
+//     } catch (error) {
+//       console.error("Error fetching country suggestions:", error);
+//     }
+//   };
+
+//   const handleSuggestionClick = (value) => {
+//     setCityFilter(value);
+//     setSuggestions([]);
+//   };
+
+//   const handleFormSubmit = (e) => {
+//     e.preventDefault();
+
+//     const filteredListings = originalListings.filter(
+//       (hotel) =>
+//         cityFilter === "" ||
+//         hotel.country.toLowerCase().includes(cityFilter.toLowerCase())
+//     );
+//     setListings(filteredListings);
+//   };
+
+//   useEffect(() => {
+//     fetch("http://127.0.0.1:8000/properties/api/property-listings/")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         if (data && data.results && Array.isArray(data.results)) {
+//           setListings(data.results);
+//           setOriginalListings(data.results);
+//         } else {
+//           console.error("Invalid data format received:", data);
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching data:", error);
+//       });
+//   }, [countryFilter]);
+
+//   useEffect(() => {
+//     if (cityFilter === "") {
+//       setListings(originalListings);
+//     }
+//   }, [cityFilter, originalListings]);
+
+//   return (
+//     <>
+//       <Navbar />
+//       <section className="accomodation_area section_gap">
+//         <div className="container">
+//           <div
+//             className="row mb-5"
+//             style={{
+//               backgroundColor: "#fffff",
+//               borderRadius: "15px",
+//               boxShadow: "2px 4px 8px rgba(0.2, 0, 0, 0.2)",
+//               padding: "20px",
+//             }}
+//           >
+//             <div className="col-md-12">
+//               <form className="form-inline" onSubmit={handleFormSubmit}>
+//                 <div className="form-group mr-1">
+//                   <input
+//                     type="text"
+//                     className="form-control"
+//                     id="location"
+//                     placeholder="Location"
+//                     value={cityFilter}
+//                     onChange={handleInputChange}
+//                   />
+//                   {/* Display suggestions */}
+//                   {suggestions.length > 0 && cityFilter !== "" && (
+//                     <>
+//                       {suggestions.length === 1 ? (
+//                         // If only one suggestion is available, allow selection
+//                         <div
+//                           onClick={() => handleSuggestionClick(suggestions[0])}
+//                         >
+//                           {suggestions[0]}
+//                         </div>
+//                       ) : (
+//                         // If multiple suggestions are available, display the dropdown
+//                         <select
+//                           className="form-control position-absolute suggestion-box"
+//                           style={{ zIndex: 100, top: "100%", left: 0 }}
+//                           size={suggestions.length > 5 ? 5 : suggestions.length}
+//                           onBlur={() => setSuggestions([])}
+//                           onChange={(e) =>
+//                             handleSuggestionClick(e.target.value)
+//                           }
+//                         >
+//                           {suggestions.map((suggestion, index) => (
+//                             <option key={index} value={suggestion}>
+//                               <strong>
+//                                 {suggestion.substring(0, cityFilter.length)}
+//                               </strong>
+//                               {suggestion.substring(cityFilter.length)}
+//                             </option>
+//                           ))}
+//                         </select>
+//                       )}
+//                     </>
+//                   )}
+//                 </div>
+//                 <div className="form-group mr-1">
+//                   <label htmlFor="checkIn">In:</label>
+//                   <input
+//                     type="date"
+//                     className="form-control ml-1"
+//                     id="checkIn"
+//                     placeholder="Enter location"
+//                   />
+//                 </div>
+//                 <div className="form-group mr-2">
+//                   <label htmlFor="checkOut">out:</label>
+//                   <input
+//                     type="date"
+//                     className="form-control ml-1"
+//                     id="checkOut"
+//                   />
+//                 </div>
+//                 <div className="form-group mr-1">
+//                   <label htmlFor="rooms">Rooms:</label>
+//                   <select className="form-control ml-1" id="rooms">
+//                     {/* Options for selecting number of rooms */}
+//                     <option value="1">1</option>
+//                     <option value="2">2</option>
+//                     <option value="3">3</option>
+//                     {/* Add more options if needed */}
+//                   </select>
+//                 </div>
+//                 <div className="form-group mr-1">
+//                   <label htmlFor="adults">Adults:</label>
+//                   <select className="form-control ml-1" id="adults">
+//                     {/* Options for selecting number of adults */}
+//                     <option value="1">1</option>
+//                     <option value="2">2</option>
+//                     <option value="3">3</option>
+//                     {/* Add more options if needed */}
+//                   </select>
+//                 </div>
+//                 <div className="form-group mr-1">
+//                   <label htmlFor="children">Children:</label>
+//                   <select className="form-control ml-1" id="children">
+//                     <option value="0">0</option>
+//                     <option value="1">1</option>
+//                     <option value="2">2</option>
+//                   </select>
+//                 </div>
+//                 <button type="submit" className="btn btn-primary">
+//                   Search
+//                 </button>
+//               </form>
+//             </div>
+//           </div>
+//           {/* Hotels Section*/}
+//           {listings.map((hotel) => (
+//             <div
+//               className="row"
+//               key={hotel.id}
+//               style={{
+//                 backgroundColor: "#ffffff",
+//                 borderRadius: "15px",
+//                 boxShadow: "2px 4px 8px rgba(0.2, 0, 0, 0.2)",
+//                 padding: "20px",
+//                 marginBottom: "20px",
+//               }}
+//             >
+//               <div className="col-md-6">
+//                 <div className="hotel_img">
+//                   <img
+//                     src={hotel.profile_image}
+//                     alt={hotel.name}
+//                     style={{ width: "60%", height: "auto" }}
+//                   />
+//                 </div>
+//               </div>
+//               <div className="col-md-6">
+//                 <div className="description">
+//                   <a href="#">
+//                     <h4 className="sec_h4">{hotel.name}</h4>
+//                   </a>
+//                   <h5>
+//                     ${hotel.cost}
+//                     <small>/night</small>
+//                   </h5>
+//                   <p>Description: {hotel.description}</p>
+//                   {/* Add other relevant information */}
+//                   <p>
+//                     Location: {hotel.location}, {hotel.city}, {hotel.country}
+//                   </p>
+//                   <p>Contact: {hotel.contact_number}</p>
+//                   {/* Add other necessary fields */}
+//                   <Link
+//                     to={`/rooms/${hotel.name}`}
+//                     className="btn btn-primary mt-2"
+//                     onClick={() => handleSelectHotel(hotel.id)}
+//                   >
+//                     View
+//                   </Link>
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </section>
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default Listing;
+
 import React, { useState, useEffect, useContext } from "react";
 import Navbar from "../components/Navbar";
 import { useLocation, Link } from "react-router-dom";
@@ -13,6 +264,8 @@ const Listing = () => {
   const [originalListings, setOriginalListings] = useState([]);
   const [cityFilter, setCityFilter] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(3); // Change this value based on your desired items per page
 
   const { selectHotel, selectedHotel } = useContext(HotelContext);
 
@@ -56,7 +309,23 @@ const Listing = () => {
         cityFilter === "" ||
         hotel.country.toLowerCase().includes(cityFilter.toLowerCase())
     );
-    setListings(filteredListings);
+    setListings(filterListingsByPage(filteredListings, 1));
+  };
+
+  const filterListingsByPage = (list, page) => {
+    const startIndex = (page - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return list.slice(startIndex, endIndex);
+  };
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    const filteredListings = originalListings.filter(
+      (hotel) =>
+        cityFilter === "" ||
+        hotel.country.toLowerCase().includes(cityFilter.toLowerCase())
+    );
+    setListings(filterListingsByPage(filteredListings, pageNumber));
   };
 
   useEffect(() => {
@@ -84,7 +353,7 @@ const Listing = () => {
   return (
     <>
       <Navbar />
-      <section className="accomodation_area section_gap">
+      <section className="accommodation_area section_gap">
         <div className="container">
           <div
             className="row mb-5"
@@ -106,18 +375,15 @@ const Listing = () => {
                     value={cityFilter}
                     onChange={handleInputChange}
                   />
-                  {/* Display suggestions */}
                   {suggestions.length > 0 && cityFilter !== "" && (
                     <>
                       {suggestions.length === 1 ? (
-                        // If only one suggestion is available, allow selection
                         <div
                           onClick={() => handleSuggestionClick(suggestions[0])}
                         >
                           {suggestions[0]}
                         </div>
                       ) : (
-                        // If multiple suggestions are available, display the dropdown
                         <select
                           className="form-control position-absolute suggestion-box"
                           style={{ zIndex: 100, top: "100%", left: 0 }}
@@ -141,46 +407,55 @@ const Listing = () => {
                   )}
                 </div>
                 <div className="form-group mr-1">
-                  <label htmlFor="checkIn">In:</label>
+                  <label htmlFor="checkIn">Check-In:</label>
                   <input
                     type="date"
                     className="form-control ml-1"
                     id="checkIn"
-                    placeholder="Enter location"
+                    placeholder="Check-In"
                   />
                 </div>
                 <div className="form-group mr-2">
-                  <label htmlFor="checkOut">out:</label>
+                  <label htmlFor="checkOut">Check-Out:</label>
                   <input
                     type="date"
                     className="form-control ml-1"
                     id="checkOut"
+                    placeholder="Check-Out"
+
                   />
                 </div>
                 <div className="form-group mr-1">
                   <label htmlFor="rooms">Rooms:</label>
-                  <select className="form-control ml-1" id="rooms">
-                    {/* Options for selecting number of rooms */}
+                  <select
+                    className="form-control ml-1"
+                    id="rooms"
+                  >
+                    <option value="0">Any</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
-                    <option value="3">3</option>
                     {/* Add more options if needed */}
                   </select>
                 </div>
                 <div className="form-group mr-1">
                   <label htmlFor="adults">Adults:</label>
-                  <select className="form-control ml-1" id="adults">
-                    {/* Options for selecting number of adults */}
+                  <select
+                    className="form-control ml-1"
+                    id="adults"
+                  >
+                    <option value="0">Any</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
-                    <option value="3">3</option>
                     {/* Add more options if needed */}
                   </select>
                 </div>
                 <div className="form-group mr-1">
                   <label htmlFor="children">Children:</label>
-                  <select className="form-control ml-1" id="children">
-                    <option value="0">0</option>
+                  <select
+                    className="form-control ml-1"
+                    id="children"
+                  >
+                    <option value="0">Any</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
                   </select>
@@ -191,7 +466,6 @@ const Listing = () => {
               </form>
             </div>
           </div>
-          {/* Hotels Section*/}
           {listings.map((hotel) => (
             <div
               className="row"
@@ -223,12 +497,10 @@ const Listing = () => {
                     <small>/night</small>
                   </h5>
                   <p>Description: {hotel.description}</p>
-                  {/* Add other relevant information */}
                   <p>
                     Location: {hotel.location}, {hotel.city}, {hotel.country}
                   </p>
                   <p>Contact: {hotel.contact_number}</p>
-                  {/* Add other necessary fields */}
                   <Link
                     to={`/rooms/${hotel.name}`}
                     className="btn btn-primary mt-2"
@@ -242,6 +514,51 @@ const Listing = () => {
           ))}
         </div>
       </section>
+      {/* Pagination */}
+      <nav className="blog-pagination justify-content-center d-flex">
+        <ul className="pagination">
+          <li className="page-item">
+            <a
+              href="#"
+              className="page-link"
+              aria-label="Previous"
+              onClick={() => paginate(currentPage - 1)}
+            >
+              <span aria-hidden="true">
+                <span className="lnr lnr-chevron-left"></span>
+              </span>
+            </a>
+          </li>
+          {[...Array(Math.ceil(originalListings.length / itemsPerPage))].map(
+            (item, index) => (
+              <li
+                key={index}
+                className={`page-item ${currentPage === index + 1 ? "active" : ""}`}
+              >
+                <a
+                  href="#"
+                  className="page-link"
+                  onClick={() => paginate(index + 1)}
+                >
+                  {index + 1}
+                </a>
+              </li>
+            )
+          )}
+          <li className="page-item">
+            <a
+              href="#"
+              className="page-link"
+              aria-label="Next"
+              onClick={() => paginate(currentPage + 1)}
+            >
+              <span aria-hidden="true">
+                <span className="lnr lnr-chevron-right"></span>
+              </span>
+            </a>
+          </li>
+        </ul>
+      </nav>
       <Footer />
     </>
   );
